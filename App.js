ebae46770016d8b008e6929b237c5f47ffa72f09
Kunal Deotale <kdeotale4@gmail.com>
2023-12-13 01:17:20 +0530

    USING USE STATE IN REACT NATIVE

--- App.js
+++ App.js
@@ -1,44 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import Day1 from "./component/Day1";
+import Day2 from "./component/Day2";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.textBold}>Learning React Native</Text>
-      </View>
-      <View style={styles.body}>
-        <Text style={styles.textBold}>
-          {/* TEXT INSIDE TEXT APPLY THE SAME STYLE PROPERTY  */}
-          This Is The Body Of App <Text>React Native</Text>{" "}
-        </Text>
-        <Text>This Is The Body Of App </Text>
-        <Text>This Is The Body Of App </Text>
-        <Text>This Is The Body Of App </Text>
-      </View>
-    </View>
+    <>
+      {/* <Day1 /> */}
+      <Day2/>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  header: {
-    backgroundColor: "teal",
-    padding: 15,
-  },
-  textBold: {
-    fontSize: 16,
-    color: "red",
-    fontWeight: "bold",
-  },
-  body: {
-    backgroundColor: "yellow",
-    margin: 10,
-    padding: 10,
-  },
-});
