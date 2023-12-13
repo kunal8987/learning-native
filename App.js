2da4df377df70ce2734a43b6106fd572543caa5f
Kunal Deotale <kdeotale4@gmail.com>
2023-12-13 23:06:26 +0530

    FLATLIST IN REACT NATIVE

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import Day1 from "./component/Day1";
 import Day2 from "./component/Day2";
 import Day3 from "./component/Day3";
 import Day4 from "./component/Day4";
+import Day5 from "./component/Day5";
 
 export default function App() {
   return (
@@ -9,7 +10,8 @@ export default function App() {
       {/* <Day1 /> */}
       {/* <Day2/> */}
       {/* <Day3/> */}
-      <Day4 />
+      {/* <Day4 /> */}
+      <Day5 />
     </>
   );
 }
