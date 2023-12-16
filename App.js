8aaac4e083eca4a055fe60a3556ff09d6dc7a716
Kunal Deotale <kdeotale4@gmail.com>
2023-12-16 18:04:17 +0530

    ADD LOADING INDICATOR TO REACT NATIVE

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import Day1 from "./component/Day1";
 import Day10 from "./component/Day10";
+import Day11 from "./component/Day11";
 import Day2 from "./component/Day2";
 import Day3 from "./component/Day3";
 import Day4 from "./component/Day4";
@@ -23,7 +24,8 @@ export default function App() {
       {/* <Day7/> */}
       {/* <Day8 /> */}
       {/* <Day9/> */}
-      <Day10/>
+      {/* <Day10/> */}
+      <Day11/>
     </>
   );
 }
