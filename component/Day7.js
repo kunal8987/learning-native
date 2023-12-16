fc3ec9b213b0f8b04274b000bfdcf06ca5534137
Kunal Deotale <kdeotale4@gmail.com>
2023-12-16 17:06:40 +0530

    ADD BACKGROUND-IMAGE TO REACT NATIVE

--- component/Day7.js
+++ component/Day7.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
 const logo = require("../assets/adaptive-icon.png");
 const Day7 = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Image source={logo} style={{ width: 300, height: 300 }} />
-      </View>
-    </SafeAreaView>
+    <View style={{ flex: 1 }}>
+      {/* <Image source={logo} style={{ width: 300, height: 300 }} /> */}
+
+      <ImageBackground source={logo} style={{ flex: 1 }}>
+        <Text style={{ margin: 25 }}>hello world </Text>
+      </ImageBackground>
+    </View>
   );
 };
 
